8d9651d3a533bc6ca90520f749dbbe6a0a09b5bd
Philipp v. Böselager <boeselager@hbz-nrw.de>
2016-01-12 13:53:17 +0100

    Explicitly call "geodata" in path (#25)

--- test/controllers/IntegrationTest.java
+++ test/controllers/IntegrationTest.java
@@ -21,7 +21,7 @@ public class IntegrationTest {
 	public void test() {
 		running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
 			public void invoke(TestBrowser browser) {
-				browser.goTo("http://localhost:3333");
+				browser.goTo("http://localhost:3333/geodata/search");
 				assertThat(browser.pageSource()).contains("lobid-geo-enrichment microservice is running");
 			}
 		});
